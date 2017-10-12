6eacbd0471f32ca78c63bb0eeae3fa74ff652da7
JJZHK <jeffcobile@gmail.com>
2017-10-12 23:37:38 +0800

    修复bug

--- tfRecordsMaker.py
+++ tfRecordsMaker.py
@@ -9,7 +9,7 @@ labels:[batch_size, label]
 data_dir:tfrecords output path
 filename:tfrecords file name
 '''
-def _convertToTFRecords(images, labels, batch_size, data_dir, filename):
+def _convertToTFRecords(images, labels, batch_size, data_dir, filename, perfilerecordcount = 2500):
     """
     Args:
         images: (num_examples, height, width, channels) np.int64 nparray (0~255)
@@ -21,22 +21,41 @@ def _convertToTFRecords(images, labels, batch_size, data_dir, filename):
     rows = images.shape[1]
     cols = images.shape[2]
     depth = images.shape[3]
+    perfileRecordNow = 0
+    fileIndex = 0
+    IsLabelsInt = True
+    if len(labels.shape) > 1: # bytes
+        IsLabelsInt = False
 
-    writer = tf.python_io.TFRecordWriter(os.path.join(data_dir, filename))
-    for index in xrange(batch_size):
+    for index in range(batch_size):
         image_raw = images[index].tostring()
+        label_raw = labels[index].tostring() if IsLabelsInt == False else labels[index]
         example = tf.train.Example(features=tf.train.Features(feature={
             'height': tf.train.Feature(int64_list=tf.train.Int64List(value=[rows])),
             'width': tf.train.Feature(int64_list=tf.train.Int64List(value=[cols])),
             'depth': tf.train.Feature(int64_list=tf.train.Int64List(value=[depth])),
-            'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[labels[index]])),
+            'label_raw' if IsLabelsInt == False else 'label': tf.train.Feature(bytes_list=tf.train.BytesList(value=[label_raw])) if IsLabelsInt == False else tf.train.Feature(int64_list=tf.train.Int64List(value=[label_raw])),
             'image_raw': tf.train.Feature(bytes_list=tf.train.BytesList(value=[image_raw]))
         }))
+
+        if perfileRecordNow == perfilerecordcount:
+            print("\r%d file closed. %d" % (fileIndex, perfileRecordNow))
+            writer.close()
+            fileIndex += 1
+            perfileRecordNow = 0
+
+        if perfileRecordNow == 0:
+            writer = tf.python_io.TFRecordWriter(os.path.join(data_dir, "%s_%d.tfrecords" % (filename, fileIndex)))
+
+
         writer.write(example.SerializeToString())
+        perfileRecordNow += 1
         percent = (index / batch_size) * 100
         if (percent % 1 == 0):
             s1 = "\rTFRecords[%s%s]%d%%"%("*"*(int(percent) + 1)," "*(100-int(percent) - 1),(int(percent) + 1))
             sys.stdout.write(s1)
             sys.stdout.flush()
-            time.sleep(0.3)
-    writer.close()
+            # time.sleep(0.3)
+    if perfileRecordNow > 0:
+        print("\r%d file closed. %d" % (fileIndex, perfileRecordNow))
+        writer.close()
--- tfRecordsReader.py
+++ tfRecordsReader.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-def readFromTFRecords(filename, batch_size, img_shape, num_threads=2, min_after_dequeue=1000):
+def readFromTFRecords(fileList, batch_size, img_shape, num_threads=2, min_after_dequeue=1000, IsLabelInt = True, LabelShape=(7,7,25)):
     def read_and_decode(filename_queue, img_shape):
         """Return a single example for queue"""
         reader = tf.TFRecordReader()
@@ -8,7 +8,7 @@ def readFromTFRecords(filename, batch_size, img_shape, num_threads=2, min_after_
             serialized_example,
             features={
                 'image_raw': tf.FixedLenFeature([], tf.string),
-                'label': tf.FixedLenFeature([], tf.int64),
+                'label' if IsLabelInt == True else 'label_raw': tf.FixedLenFeature([], tf.int64 if IsLabelInt == True else tf.string),
             }
         )
         # some essential steps
@@ -17,11 +17,15 @@ def readFromTFRecords(filename, batch_size, img_shape, num_threads=2, min_after_
         image.set_shape(img_shape)
         image = tf.cast(image, tf.float32) * (1 / 255.0)  # set to [0, 1]
 
-        sparse_label = tf.cast(features['label'], tf.int32)
-
+        if IsLabelInt == True:
+            sparse_label = tf.cast(features['label'], tf.int32)
+        else:
+            sparse_label = tf.decode_raw(features['label_raw'], tf.float32)
+            sparse_label = tf.reshape(sparse_label, LabelShape)
+        #
         return image, sparse_label
 
-    filename_queue = tf.train.string_input_producer([filename])
+    filename_queue = tf.train.string_input_producer(fileList)
 
     image, sparse_label = read_and_decode(filename_queue, img_shape) # share filename_queue with multiple threads
 
